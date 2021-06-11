700d4a9ab149efe118d8d4ffd1c5eaea0a0b3521
Susmit Kumar Mishra <susmitmishra125@gmail.com>
2021-06-11 21:26:12 +0530

    changed upload folder

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 # UPLOAD_FOLDER = r'C:\Users\susmi\OneDrive\Documents\GitHub\aerial-cactus-identification'
+UPLOAD_FOLDER = r''
+
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 # app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
--- index.py
+++ index.py
@@ -8,27 +8,28 @@ import os
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+		return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def submit_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            getPrediction(filename)
-            output = getPrediction(filename)
-            flash(output)
-            return redirect('/')
+		if request.method == 'POST':
+				if 'file' not in request.files:
+						flash('No file part')
+						return redirect(request.url)
+				file = request.files['file']
+				if file.filename == '':
+						flash('No file selected for uploading')
+						return redirect(request.url)
+				if file:
+						filename = secure_filename(file.filename)
+						file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+						getPrediction(filename)
+						output = getPrediction(filename)
+						flash(output)
+						os.remove(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+						return redirect('/')
 
 
 if __name__ == "__main__":
-    app.run()
+		app.run()
